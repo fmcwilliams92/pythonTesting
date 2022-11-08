0f7f694ef0221841d12f61284fc5cd22c41675d5
Fred McWilliams <fredmcwilliams92@gmail.com>
2022-11-08 06:45:22 -0600

    madlib python test

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,10 @@
-# this prints some txt
-fred_Age = 30
-dane_Age = 31
+#this is a comment in python, similar to most language
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famouns Person: ")
 
-print(fred_Age==dane_Age);
+madlib = f"Computer programming is so {adj}! It makes me so excited and fulfilled. \
+    I love {verb1}. Lets kill this shit togother and become {verb2} like {famous_person}!"
+
+print(madlib);
